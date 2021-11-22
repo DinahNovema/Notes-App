6a584ce4e34e3b793eba7b5112c007bd395edef8
dinah <dinahnovema18@gmail.com>
2021-11-22 18:54:28 +0200

    Notes App updated

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import Modal from "react-modal";
 import { FaTrashAlt } from "react-icons/fa";
 import { FaEdit } from "react-icons/fa";
